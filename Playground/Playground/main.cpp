fefaad35c0cee04879c683b827720e4635e9226d
henrykalex <Enrique@MacBook-Pro-de-Enrique-3.local>
2017-01-18 09:30:15 -0600

    Initial commit.

---
+++ Playground/Playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Playground
+//
+//  Created by Henry on 18/01/17.
+//  Copyright © 2017 Henryk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
